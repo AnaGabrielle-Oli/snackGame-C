71e5e9f1ae5c2c945efeda63e28a9521a1335c5b
Ana Gabrielle <anagabrielledasilvaoliveira@gmail.com>
2026-06-08 13:59:42 +0000

    matriz iniciada

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char matriz[13][13];
+
+    for (int i = 0; i < 12; i++){
+        matriz[0][i];
+    }
+    /*
+    printf("————————————\n");
+    printf("|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n");
+    printf("————————————");*/
+
+    return 0;
+}
